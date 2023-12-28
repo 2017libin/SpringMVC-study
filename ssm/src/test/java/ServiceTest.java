4a24e0ca2de7def59f8ef68f444a1fe2abeeb37e
2017libin <1963482624@11.com>
2023-12-28 12:16:00 +0800

    add ssm

---
+++ ssm/src/test/java/ServiceTest.java
@@ -0,0 +1,7 @@
+public class ServiceTest {
+//    public String list() {
+//        BookService bookService = new BookServiceImpl();
+//        List<Books> list = bookService.queryAllBook();
+//        System.out.println(list);
+//    }
+}
